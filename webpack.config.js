5b631fa2bd9091f6ba91db1ea7d00acadf02d580
mtoutside <more.humor.or@gmail.com>
2022-06-27 02:26:12 +0900

    wip update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,42 @@
-const webpack = require('webpack');
+const { webpack } = require("webpack-stream"); // Webpack 読み込み
+const path = require("path");
 
 module.exports = (env = "dev") => {
   return {
+    target: ["web", "es5"], // Webpack 5 からの ES5 ( IE11 等 ) 向け設定
     mode: "production",
+    devtool: "inline-source-map",
+    resolve: {
+      extensions: [".js"], // require する際に、拡張子を省略するための設定
+    },
     entry: "./src/assets/js/script.js",
     output: {
-      path: `${__dirname}/dist/assets/js/`,
-      filename: "script.js"
+      path: path.resolve(__dirname, "dist/assets/js/"),
+      filename: "script.js",
     },
     module: {
       rules: [
         {
           test: /\.js$/,
+          use: [
+            {
+              options: {
+                presets: ["@babel/preset-env"],
+                plugins: [
+                  ["@babel/plugin-transform-classes", { loose: true }], // ES6 を ES5 に変換
+                ],
+                cacheDirectory: true,
+              },
+            },
+          ],
           exclude: /node_module/,
-          loader: "babel-loader",
-        },
-        {
-          enforce: "pre",
-          test: /\.js$/,
-          exclude: /node_module/,
-          options: {
-            fix: true,
-          },
         },
       ],
     },
     plugins: [
       new webpack.DefinePlugin({
-        CONFIG: JSON.stringify({ env })
-      })
-    ]
+        CONFIG: JSON.stringify({ env }),
+      }),
+    ],
   };
 };
